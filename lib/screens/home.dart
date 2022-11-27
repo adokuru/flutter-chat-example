0ac8d1f51ee14b7a003d81c378d7a305649673ed
David Adokuru <davidadokuru@gmail.com>
2022-11-27 13:55:42 +0100

    updates

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter_socket_io/model/message.dart';
 import 'package:flutter_socket_io/providers/home.dart';
